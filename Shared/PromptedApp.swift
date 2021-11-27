6399c14af000e7249f138340dd41dfb380f6f803
Sarah Lichter <sarahlichter@Sarahs-MacBook-Pro-2.local>
2021-11-26 19:34:50 -0600

    initial project

---
+++ Shared/PromptedApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromptedApp.swift
+//  Shared
+//
+//  Created by Sarah Lichter on 11/26/21.
+//
+
+import SwiftUI
+
+@main
+struct PromptedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
